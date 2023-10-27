8364e98bb598eb61462b42440f8b853b84847f8e
hwang seung joon <asherolleh@gmail.com>
2023-10-27 23:44:04 +0900

    setting for project

---
+++ src/main/kotlin/com/example/alom1/finiensserver/FiniensServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.alom1.finiensserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FiniensServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FiniensServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/alom1/finiensserver/FiniensServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.alom1.finiensserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FiniensServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
