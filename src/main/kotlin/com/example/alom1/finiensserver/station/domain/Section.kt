c610321d9c9d0226702a7bd1b9bb8599c4fabb3d
hwang seung joon <asherolleh@gmail.com>
2023-11-09 21:35:21 +0900

    add Section entity

---
+++ src/main/kotlin/com/example/alom1/finiensserver/station/domain/Section.kt
@@ -0,0 +1,36 @@
+package com.example.alom1.finiensserver.station.domain
+
+import com.example.alom1.finiensserver.core.PrimaryKeyStaticEntity
+import com.example.alom1.finiensserver.line.domain.Line
+import jakarta.persistence.*
+
+@Entity
+@Table(name="subway_station_section")
+class Section(
+    distance: Double,
+    duration: Int,
+    upStation: Station,
+    downStation: Station,
+    line: Line
+): PrimaryKeyStaticEntity() {
+    @Column(nullable = false)
+    var distance: Double = distance
+        protected set
+
+    @Column(nullable = false)
+    var duration: Int = duration
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    var upStation: Station = upStation
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    var downStation: Station = downStation
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    var line: Line = line
+}
