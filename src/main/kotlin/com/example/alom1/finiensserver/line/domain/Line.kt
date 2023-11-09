1e40eb6899dad371f16414c1655bf291f895823b
hwang seung joon <asherolleh@gmail.com>
2023-11-09 15:57:35 +0900

    line entity 생성

---
+++ src/main/kotlin/com/example/alom1/finiensserver/line/domain/Line.kt
@@ -0,0 +1,16 @@
+package com.example.alom1.finiensserver.line.domain
+
+import com.example.alom1.finiensserver.core.PrimaryKeyStaticEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name="subway_line")
+class Line(
+    lineName: String,
+): PrimaryKeyStaticEntity() {
+    @Column(name = "line_name", nullable = false)
+    var lineName: String = lineName
+        protected set
+}
