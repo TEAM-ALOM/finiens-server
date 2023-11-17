c19400ff472ea605c80c97a4827a9404ec4ed1ac
hwang seung joon <asherolleh@gmail.com>
2023-11-17 14:27:53 +0900

    category search 기능 추가

--- src/main/kotlin/com/example/alom1/finiensserver/data/destination/DestinationRepositoryImpl.kt
+++ src/main/kotlin/com/example/alom1/finiensserver/data/destination/DestinationRepositoryImpl.kt
@@ -4,6 +4,10 @@ import com.example.alom1.finiensserver.data.core.RestTemplateClient
 import com.example.alom1.finiensserver.domain.destination.Place
 import com.example.alom1.finiensserver.domain.core.Coordinate
 import com.example.alom1.finiensserver.domain.destination.DestinationRepository
+import com.fasterxml.jackson.databind.type.CollectionLikeType
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.json.JSONArray
+import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.*
@@ -16,14 +20,21 @@ class DestinationRepositoryImpl @Autowired constructor(
     @Value("\${spring.search.url}")
     private val url: String,
 ): DestinationRepository, RestTemplateClient(key=key) {
-    private val baseUrl = "https://dapi.kakao.com/v2/local/"
-
+    private val mapper = jacksonObjectMapper()
+    private val listPlaceType: CollectionLikeType = mapper.typeFactory.constructCollectionLikeType(List::class.java, DestinationResponseDto::class.java)
 
     override fun findDestinationByKeyWord(keyWord: String, coordinate: Coordinate):List<Place> {
-        val urlWithParam: String = "$url?$keyWord&x=${coordinate.longitude}&y=${coordinate.longitude}"
+        val urlWithParam: String = "$url?query=$keyWord&x=${coordinate.longitude}&y=${coordinate.latitude}"
 
-        val result: List<Place> = get<List<Place>>(urlWithParam)
+        val jsonResult : JSONObject = get(urlWithParam)
+        return jsonObjectToEntity(jsonObject = jsonResult)
+    }
 
-        return result
+    private fun jsonObjectToEntity(jsonObject: JSONObject): List<Place> {
+        val jsonArray: JSONArray = jsonObject.getJSONArray("documents")
+
+        val dto = mapper.readValue<List<DestinationResponseDto>>(jsonArray.toString(), listPlaceType)
+
+        return dto.map { d -> d.toEntity()}
     }
 }
