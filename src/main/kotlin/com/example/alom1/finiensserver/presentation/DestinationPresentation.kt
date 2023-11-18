a5fd5d25c4b3c7e1dd228e23f7fd975fff4e291a
hwang seung joon <asherolleh@gmail.com>
2023-11-18 14:55:55 +0900

    add destination presentation class

---
+++ src/main/kotlin/com/example/alom1/finiensserver/presentation/DestinationPresentation.kt
@@ -0,0 +1,27 @@
+package com.example.alom1.finiensserver.presentation
+
+import com.example.alom1.finiensserver.domain.core.Coordinate
+import com.example.alom1.finiensserver.domain.destination.DestinationService
+import com.example.alom1.finiensserver.domain.destination.Place
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DestinationPresentation @Autowired constructor(
+    private val destinationService: DestinationService
+) {
+
+    @GetMapping("/places/destination")
+    fun findDestinations(
+        @RequestParam("place-name") placeName: String,
+        @RequestParam latitude: Double,
+        @RequestParam longitude: Double
+    ): List<Place> {
+        return destinationService.findDestinationByKeyWord(
+            keyWord = placeName,
+            coordinate = Coordinate(latitude=latitude, longitude=longitude)
+        )
+    }
+}
