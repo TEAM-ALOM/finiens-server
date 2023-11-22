e45569cc6c3f6589394d7b5ac689df5ff3cd1a5b
hwang seung joon <asherolleh@gmail.com>
2023-11-22 21:31:49 +0900

    add osrm repository 추가 자세한 경로 정보 알려 주는 함수 추가 간단한 경로 알려 주는 함수 추가

---
+++ src/main/kotlin/com/example/alom1/finiensserver/data/path/OSRMRepository.kt
@@ -0,0 +1,41 @@
+package com.example.alom1.finiensserver.data.path
+
+import com.example.alom1.finiensserver.data.core.RestTemplateClient
+import com.example.alom1.finiensserver.domain.core.Coordinate
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.json.JSONArray
+import org.json.JSONObject
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Repository
+
+@Repository
+class OSRMRepository @Autowired constructor(
+
+) : RestTemplateClient() {
+    val baseUrl: String = "http://router.project-osrm.org/route/v1/"
+//    val basicPathUrl: String = baseUrl + "foot/"
+
+
+    fun getDetailedPathInfo(departureCoordinate: Coordinate, destinationCoordinate: Coordinate) {
+        val url : String = baseUrl + "foot/" +
+                "${departureCoordinate.longitude}," +
+                "${departureCoordinate.latitude};" +
+                "${destinationCoordinate.longitude}," +
+                "${destinationCoordinate.latitude}" +
+                "?overview=full&geometries=geojson"
+
+        val dto: DetailedPathInfoDto = DetailedPathInfoDto.fromJson(get(url))
+    }
+
+    fun getBasicPathInfo(departureCoordinate: Coordinate, destinationCoordinate: Coordinate) : BasicPathInfoDto {
+        val url : String = baseUrl + "foot/" +
+                "${departureCoordinate.longitude}," +
+                "${departureCoordinate.latitude};" +
+                "${destinationCoordinate.longitude}," +
+                "${destinationCoordinate.latitude}" +
+                "?overview=false&geometries=geojson"
+
+        val dto : BasicPathInfoDto = BasicPathInfoDto.fromJson(get(url))
+        return dto
+    }
+}
