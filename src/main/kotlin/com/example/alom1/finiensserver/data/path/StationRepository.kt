dbae8ee250f3a76ba4fb7234e1066821d5e015e8
hwang seung joon <asherolleh@gmail.com>
2023-11-29 22:33:42 +0900

    fix 1개 목적지에서 1개의 정류장 찾는 함수에서 2개 장소에 대해 각각의 가장 가까운 정류장을 찾는 쿼리문으로 변경

--- src/main/kotlin/com/example/alom1/finiensserver/data/path/StationRepository.kt
+++ src/main/kotlin/com/example/alom1/finiensserver/data/path/StationRepository.kt
@@ -8,17 +8,24 @@ import org.springframework.stereotype.Repository
 @Repository
 interface StationRepository: JpaRepository<Station, Long> {
     @Query(
-        value = "SELECT s.*, " +
-                "(6371 * acos( cos( radians(:inputLatitude) ) " +
-                "* cos( radians( s.latitude ) ) " +
-                "* cos( radians( s.longitude ) - radians(:inputLongitude) ) " +
-                "+ sin( radians(:inputLatitude) ) " +
-                "* sin( radians( s.latitude ) ) ) ) " +
-                "AS distance " +
-                "FROM subway_station AS s " +
-                "ORDER BY distance " +
-                "LIMIT :number",
+        value = "SELECT s.* from subway_station as s " +
+                "where s.id = " +
+                "(SELECT s1.id FROM subway_station as s1 ORDER BY " +
+                "(POW((69.1 * (s1.latitude - :departureLatitude)), 2) + POW((69.1 * (s1.longitude - :departureLongitude) * COS(s1.latitude / 57.3)), 2)) " +
+                "LIMIT :number) " +
+                "UNION ALL " +
+                "(SELECT s.* from subway_station as s " +
+                "where s.id = " +
+                "(SELECT s2.id FROM subway_station as s2 ORDER BY " +
+                "(POW((69.1 * (s2.latitude - :destinationLatitude)), 2) + POW((69.1 * (s2.longitude - :destinationLongitude) * COS(s2.latitude / 57.3)), 2)) " +
+                "LIMIT :number));",
         nativeQuery = true
     )
-    fun findClosestStation(inputLatitude:Double, inputLongitude: Double, number : Int = 1) : Station
+    fun findClosestStations(
+        departureLatitude:Double,
+        departureLongitude: Double,
+        destinationLatitude:Double,
+        destinationLongitude: Double,
+        number : Int = 1
+    ) : List<Station>
 }
